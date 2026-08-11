f1d9a66a861cae51c193a7fc10eb911c36f6b28e
Mahdi <mahdiazimi.885@gmail.com>
2026-08-11 05:50:49 +0330

    feat : PublisherRepositoryImpl

---
+++ src/main/java/ir/maktabsharif/repository/impl/publisher/PublisherRepository.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.repository.impl.publisher;
+
+import ir.maktabsharif.model.Publisher;
+import ir.maktabsharif.repository.GenericRepository;
+
+public interface PublisherRepository extends GenericRepository<Publisher> {
+}
---
+++ src/main/java/ir/maktabsharif/repository/impl/publisher/PublisherRepositoryImpl.java
@@ -0,0 +1,55 @@
+package ir.maktabsharif.repository.impl.publisher;
+
+import ir.maktabsharif.exception.BookNotFoundException;
+import ir.maktabsharif.model.Publisher;
+import ir.maktabsharif.util.HibernateUtil;
+
+import java.util.Optional;
+
+public class PublisherRepositoryImpl implements PublisherRepository{
+
+    @Override
+    public Long save(Publisher publisher) {
+        HibernateUtil.inTxResult(em -> {
+            em.persist(publisher);
+            return publisher;
+        });
+        return publisher.getId();
+    }
+
+    @Override
+    public boolean update(Publisher publisher) {
+        Publisher publisher1 = HibernateUtil.inTxResult( em -> {
+            Publisher publisher2 = em.find(Publisher.class , publisher.getId());
+            if(publisher2 == null){
+                return null;
+            }
+            publisher2.setName(publisher.getName());
+            publisher2.setBooks(publisher.getBooks());
+
+            return publisher2;
+        });
+        return publisher1 != null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+
+        HibernateUtil.inTxResult(eM -> {
+            Publisher publisher = eM.find(Publisher.class, id);
+            if (publisher == null) {
+                return false;
+            }
+            eM.remove(publisher);
+            return null;
+        });
+        return true;
+    }
+
+    @Override
+    public Optional<Publisher> findById(Long id) {
+        return Optional.ofNullable(HibernateUtil.inTxResult(eM ->
+                eM.find(Publisher.class, id)
+        ));
+    }
+}
